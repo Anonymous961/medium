2f33a4eed5b0e270029117584575a9709e8138ac
Anil Kumar Behera <ark845612@gmail.com>
2024-06-07 00:02:24 +0530

    frontend: singin signup pages

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,123 @@
+import { SignupInput } from "@anonymous961/medium-common";
+import axios from "axios";
+import { ChangeEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../config";
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const [postInputs, setPostInputs] = useState<SignupInput>({
+    name: "",
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  async function sendRequest() {
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/api/v1/user/${type === "signin" ? "signin" : "signup"}`,
+        postInputs
+      );
+      const jwt = response.data.token;
+      localStorage.setItem("token", jwt);
+      navigate("/blogs");
+    } catch (error) {
+      alert("Error while signing up");
+      console.log(error);
+    }
+  }
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10 text-center">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-400">
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+
+              <Link
+                className="pt-2 underline"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Sign Up" : "Sign In"}
+              </Link>
+            </div>
+          </div>
+          <div className="pt-8">
+            {type === "signup" && (
+              <LabelledInput
+                label="Name"
+                placeholder="Anil Kumar"
+                onChange={(e) => {
+                  setPostInputs((c) => ({
+                    ...c,
+                    name: e.target.value,
+                  }));
+                }}
+              />
+            )}
+            <LabelledInput
+              label="Username"
+              placeholder="anil@cloudfare.com"
+              onChange={(e) => {
+                setPostInputs((c) => ({
+                  ...c,
+                  username: e.target.value,
+                }));
+              }}
+            />
+            <LabelledInput
+              label="Password"
+              placeholder="nx389SY&fg&3%*47#&*$v"
+              type="password"
+              onChange={(e) => {
+                setPostInputs((c) => ({
+                  ...c,
+                  password: e.target.value,
+                }));
+              }}
+            />
+            <button
+              onClick={sendRequest}
+              type="button"
+              className="w-full mt-8 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign Up" : "Sign In"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+
+function LabelledInput({
+  label,
+  placeholder,
+  onChange,
+  type,
+}: LabelledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm font-semibold text-black pt-4">
+        {label}
+      </label>
+      <input
+        onChange={onChange}
+        type={type || "text"}
+        id="first_name"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.ark845612.workers.dev"
